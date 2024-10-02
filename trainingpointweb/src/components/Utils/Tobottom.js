6be341a94036abeff273847e527c7b25473352e3
Phạm Ngọc Trường Sơn <sonpnts@ou.edu.vn>
2024-10-02 10:36:22 +0700

    Update Tobottom.js

--- trainingpointweb/src/components/Utils/Tobottom.js
+++ trainingpointweb/src/components/Utils/Tobottom.js
@@ -1,9 +1,3 @@
-// export const isCloseToBottom = ({layoutMeasurement, contentOffset, contentSize}) => {
-//     const paddingToBottom = 10;
-//     return layoutMeasurement.height + contentOffset.y >=
-//       contentSize.height - paddingToBottom;
-// };
-
 export const isCloseToBottom = (target) => {
   if (target) {
       const paddingToBottom = 10;
